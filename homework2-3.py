788c59d4a1c6b1fd2759467f8bb863c289f0ce8f
Islam9600 <islamfufuza78@gmail.com>
2025-04-17 15:28:26 +0600

    Удалил файл

--- homework2-3.py
+++ homework2-3.py
@@ -1,46 +1,55 @@
 import flet as ft
-from datetime import datetime
 
 def main(page: ft.Page):
     page.title = "Моё первое приложение"
     page.theme_mode = ft.ThemeMode.LIGHT
     greeting_text = ft.Text("Hello world!")
 
+    HISTORY_FILE = "history.txt"
+
     greeting_history = []
+
+    def load_history():
+        try:
+            with open(HISTORY_FILE, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                for line in lines:
+                    name = line.strip()
+                    if name:
+                        greeting_history.append(name)
+                history_text.value = 'История приветствий:\n' + "\n".join(greeting_history)
+                page.update()
+        except FileNotFoundError:
+            pass 
+
+    def save_history():
+        with open(HISTORY_FILE, 'w', encoding='utf-8') as file:
+            for name in greeting_history:
+                file.write(name + "\n")
 
     history_text = ft.Text("История приветствий:", size="bodyMedium")
 
-    def get_greeting(name):
-        hour = datetime.now().hour
-        if 6 <= hour < 12:
-            return f"Доброе утро, {name}!"
-        elif 12 <= hour < 18:
-            return f"Добрый день, {name}!"
-        elif 18 <= hour < 24:
-            return f"Добрый вечер, {name}!"
-        else:
-            return f"Доброй ночи, {name}!"
+
 
     def on_button_click(_):
         name = name_input.value.strip()
 
         if name:
-            greeting = get_greeting(name)
-            greeting_text.value = greeting
+            greeting_text.value = f"Привет, {name}!"
             greet_button.text = "Поздороваться снова"
             name_input.value = ""
 
-            # Формируем строку: дата и время слева, имя справа
-            full_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            greeting_history.append(f"{full_time:<20} | {name}")
-            history_text.value = "История приветствий:\n" + "\n".join(greeting_history)
+            greeting_history.append(name)
+            history_text.value = "История приветсвий:\n" + "\n".join(greeting_history)
+            save_history()
         else:
             greeting_text.value = 'Пожалуйста, введите имя ❌'
             
         page.update()
 
+
     name_input = ft.TextField(label="Введите имя", autofocus=True,
-                              on_submit=on_button_click)
+                                on_submit=on_button_click)
 
     def clear_history(_):
         greeting_history.clear()
@@ -48,38 +57,33 @@ def main(page: ft.Page):
         page.update()
 
     def toggle_theme(_):
-        page.theme_mode = (
-            ft.ThemeMode.DARK if page.theme_mode == ft.ThemeMode.LIGHT
-            else ft.ThemeMode.LIGHT
-        )
+        if page.theme_mode == ft.ThemeMode.LIGHT:
+            page.theme_mode = ft.ThemeMode.DARK
+        else:
+            page.theme_mode = ft.ThemeMode.LIGHT
+        
         page.update()
 
-    theme_button = ft.IconButton(
-        icon=ft.icons.BRIGHTNESS_6,
-        tooltip='Сменить тему',
-        on_click=toggle_theme
-    )
+    theme_button = ft.IconButton(icon=ft.icons.BRIGHTNESS_6, 
+                                 tooltip='Сменить тему',
+                                 on_click=toggle_theme)
 
-    greet_button = ft.ElevatedButton(
-        'Поздороваться',
-        on_click=on_button_click,
-        icon=ft.icons.HANDSHAKE
-    )
+    greet_button = ft.ElevatedButton('Поздороваться', 
+                                    on_click=on_button_click,
+                                    icon=ft.icons.HANDSHAKE)
     
-    clear_button = ft.TextButton("Очистить историю", on_click=clear_history)
+    clear_button = ft.TextButton("Очистить исорию", icon=ft.icons.DELETE_SWEEP, on_click=clear_history)
 
-    clear_button_2 = ft.IconButton(
-        icon=ft.icons.DELETE,
-        tooltip='Очистить историю',
-        on_click=clear_history
-    )
+    clear_button_2 = ft.IconButton(icon=ft.icons.DELETE,
+                                    tooltip='Очистить историю',
+                                    on_click=clear_history)
 
-    page.add(
-        ft.Row([theme_button, clear_button, clear_button_2], alignment=ft.MainAxisAlignment.CENTER),
-        greeting_text,
-        name_input,
-        greet_button,
-        history_text
-    )
+    # page.add(theme_button, greeting_text, name_input, greet_button, history_text, clear_button)
+    page.add(ft.Row([theme_button, clear_button, clear_button_2],  alignment=ft.MainAxisAlignment.CENTER),
+                    greeting_text,
+                    name_input,
+                    greet_button,
+                    history_text)
+    load_history()
 
 ft.app(main)
